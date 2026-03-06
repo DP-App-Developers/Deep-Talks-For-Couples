41291173f087b3ee8f896e4305217aa5fee79730
Dennis Hong <dehong@ebay.com>
2026-03-05 16:34:21 -0800

    lock to portrait for width < 600dp

--- app/src/main/java/com/dpappdev/deeptalkscouples/MainActivity.kt
+++ app/src/main/java/com/dpappdev/deeptalkscouples/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.dpappdev.deeptalkscouples
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +14,17 @@ import com.dpappdev.deeptalkscouples.ui.theme.DeepTalksCouplesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Lock to portrait orientation for narrow devices (phones)
+        // Tablets (sw600dp+) can rotate freely
+        val configuration = resources.configuration
+        val screenWidthDp = configuration.screenWidthDp
+        
+        if (screenWidthDp < 600) {
+            // Narrow device (phone) - lock to portrait
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        
         enableEdgeToEdge()
         setContent {
             DeepTalksCouplesTheme {
